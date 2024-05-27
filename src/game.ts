ca8862d071713700406b51ba8fbc314c4bc39ec3
lucas <lucasmunizteles@gmail.com>
2024-05-27 14:58:02 -0300

    alterações no gamets

--- src/game.ts
+++ src/game.ts
@@ -3,18 +3,19 @@ import {Cena1} from "./scenes/Cena1";
 import {Menu} from "./scenes/Menu";
 import {Gameover} from "./scenes/Gameover";
 import {Tutorial} from "./scenes/Tutorial";
+import { Name } from "./scenes/Name";
 
 const config = {
   type: Phaser.CANVAS, // auto
   backgroundColor: "#125555",
   width: 1200,
   height: 640,
-  scene: [Cena1,Gameover],
+  scene: [Menu,Tutorial,Cena1,Gameover],
   physics:{
     default: "arcade",
     arcade:{
       gravity:{y:850}, //850
-      debug:true
+      //debug:true
     }
   },
   pixelArt: true
